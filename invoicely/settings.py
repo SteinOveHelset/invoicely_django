d0f0fbe2fdca57efec2cebefdc76939fabb39f08
Code With Stein <codewithstein@Steins-MBP.home>
2021-02-08 21:29:33 +0100

    part 2

--- invoicely/settings.py
+++ invoicely/settings.py
@@ -34,10 +34,10 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
